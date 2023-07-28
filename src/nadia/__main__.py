be331ac568b97549cc7df65e177928ea548808f8
Davi Romero <daviromero@insighlab.ufc.br>
2023-07-28 11:17:24 -0300

    ajustes

--- src/nadia/__main__.py
+++ src/nadia/__main__.py
@@ -3,7 +3,6 @@ from nadia.nadia_pt_fo import check_proof
 
 parser = argparse.ArgumentParser(description='NADIA - Natural Deduction Proof Assistant.')
 parser.add_argument("-i", type=str,help="Arquivo de entrada com a prova em NADIA.")
-parser.add_argument("-o", type=str,help="Arquivo de saída do resultado da verificação da prova na NADIA")
 args = parser.parse_args()
 fileName = 'example_nadia.txt'
 if args.i is not None: fileName = args.i
@@ -11,9 +10,12 @@ if args.i is not None: fileName = args.i
 
 def app(fileName):
     f = open(fileName, 'r')
+    print(f)
+    if f==None:
+        return "Arquivo não encontradao"
 
     input_proof = f.read()
 
-    return check_proof(input_proof)
+    return check_proof(input_proof,show_fitch=False,show_gentzen=False)
 
 print(app(fileName))
