327ab553a3d140fdb06b07612ed97543cd0d64a6
Davi Romero <daviromero@insighlab.ufc.br>
2023-07-29 10:10:28 -0300

    ajustes no check_proof

--- src/nadia/__main__.py
+++ src/nadia/__main__.py
@@ -4,11 +4,11 @@ import os
 
 parser = argparse.ArgumentParser(description='NADIA - Natural Deduction Proof Assistant.')
 parser.add_argument("-i", type=str, required=True, help="Arquivo de entrada com a prova em NADIA.")
-parser.add_argument("-t", type=str,default=None, help="Entre com o teorema a ser analisado.")
+parser.add_argument("-t", type=str, help="Entre com o teorema a ser analisado.")
 args = parser.parse_args()
+input_theorem = None
 if args.i is not None: fileName = args.i
 if args.t is not None: input_theorem = args.t
-print("teste 0")
 
 
 def app(fileName, input_theorem):
@@ -19,5 +19,4 @@ def app(fileName, input_theorem):
     input_proof = f.read()
     return check_proof(input_proof,input_theorem=input_theorem,show_fitch=False,show_gentzen=False)
 
-print("teste 1")
 print(app(fileName,input_theorem))
